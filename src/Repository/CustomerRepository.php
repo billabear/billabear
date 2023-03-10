2f6b292298375aea6e2bd376755df59a168e0c4f
Iain <iain@humblyarrogant.io>
2023-03-10 20:51:52 +0100

    Fix cs

--- src/Repository/CustomerRepository.php
+++ src/Repository/CustomerRepository.php
@@ -2,14 +2,14 @@
 
 namespace App\Repository;
 
- use Parthenon\Billing\Entity\Subscription;
- use Parthenon\Common\Repository\DoctrineRepository;
- use Parthenon\User\Entity\UserInterface;
+use Parthenon\Billing\Entity\Subscription;
+use Parthenon\Common\Repository\DoctrineRepository;
+use Parthenon\User\Entity\UserInterface;
 
- class CustomerRepository extends DoctrineRepository implements CustomerRepositoryInterface
- {
-     public function getSubscriptionForUser(UserInterface $user): Subscription
-     {
-         return new Subscription();
-     }
- }
+class CustomerRepository extends DoctrineRepository implements CustomerRepositoryInterface
+{
+    public function getSubscriptionForUser(UserInterface $user): Subscription
+    {
+        return new Subscription();
+    }
+}
