10203f6ed0ee43b12aaf283de1a96738c3f49ed9
Iain <iain@humblyarrogant.io>
2023-03-16 18:25:21 +0100

    Fix copyright header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
@@ -8,7 +18,7 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
-        'header_comment' => ['header' => 'Copyright Iain Cambridge 2020-2023.
+        'header_comment' => ['header' => 'Copyright Iain Cambridge 2023.
                 
 Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
 
