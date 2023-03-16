15f082395dfe93b75bb8b0e40cb05c24040e090e
Iain <iain@humblyarrogant.io>
2023-03-16 18:19:58 +0100

    add copyright header

--- tests/Behat/UserContext.php
+++ tests/Behat/UserContext.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
 namespace App\Tests\Behat;
 
 use App\Entity\ForgotPasswordCode;
