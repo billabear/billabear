bdf7bbe1f55a6a682f1901804a5fec9d8defb3b7
Iain <iain@humblyarrogant.io>
2023-03-31 15:26:09 +0200

    Tweak menu css

--- assets/components/app/AppLogo.vue
+++ assets/components/app/AppLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full text-center">
+  <div class="w-full text-center mb-5">
     <img src="/images/app-logo.png" alt="" class="m-auto" width="175" />
   </div>
 </template>
