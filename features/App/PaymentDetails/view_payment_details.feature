1967b47088b4ac626c236c89978acb5c0c2fe798
Iain <iain@humblyarrogant.io>
2023-04-02 11:47:09 +0200

    Add test for view payment details

---
+++ features/App/PaymentDetails/view_payment_details.feature
@@ -0,0 +1,23 @@
+Feature: Customer Read APP
+  In order to manage a customer
+  As an APP user
+  I need to be see what the customer info
+
+  Background:
+    Given the following accounts exist:
+      | Name        | Email                   | Password  |
+      | Sally Brown | sally.brown@example.org | AF@k3P@ss |
+      | Tim Brown   | tim.brown@example.org   | AF@k3P@ss |
+      | Sally Braun | sally.braun@example.org | AF@k3Pass |
+
+  Scenario: Get customer info
+    When I have logged in as "sally.brown@example.org" with the password "AF@k3P@ss"
+    And the follow customers exist:
+      | Email                    | Country | External Reference | Reference    |
+      | customer.one@example.org | DE      | cust_jf9j545       | Customer One |
+      | customer.two@example.org | UK      | cust_dfugfdu       | Customer Two |
+    And the following payment details:
+      | Customer Email           | Last Four | Expiry Month | Expiry Year |
+      | customer.one@example.org | 0444      | 03           | 25          |
+    When I view the customer info via the site for "customer.one@example.org"
+    Then I will see 1 payment details
---
+++ tests/Behat/PaymentDetails/AppContext.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Tests\Behat\PaymentDetails;
+
+use App\Repository\Orm\CustomerRepository;
+use App\Tests\Behat\Customers\CustomerTrait;
+use App\Tests\Behat\SendRequestTrait;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Session;
+use Parthenon\Billing\Entity\PaymentDetails;
+use Parthenon\Billing\Repository\Orm\PaymentDetailsServiceRepository;
+
+class AppContext implements Context
+{
+    use SendRequestTrait;
+    use CustomerTrait;
+
+    public function __construct(
+        private Session $session,
+        private PaymentDetailsServiceRepository $paymentDetailsRepository,
+        protected CustomerRepository $customerRepository
+    ) {
+    }
+
+    /**
+     * @When the following payment details:
+     */
+    public function theFollowingPaymentDetails(TableNode $table)
+    {
+        $rows = $table->getColumnsHash();
+
+        foreach ($rows as $row) {
+            $customer = $this->getCustomerByEmail($row['Customer Email']);
+            $paymentDetails = new PaymentDetails();
+            $paymentDetails->setName('One');
+            $paymentDetails->setBrand('dummy');
+            $paymentDetails->setLastFour($row['Last Four']);
+            $paymentDetails->setExpiryMonth($row['Expiry Month']);
+            $paymentDetails->setExpiryYear($row['Expiry Year']);
+            $paymentDetails->setStoredPaymentReference(bin2hex(random_bytes(32)));
+            $paymentDetails->setStoredCustomerReference($customer->getExternalCustomerReference());
+            $paymentDetails->setCustomer($customer);
+            $paymentDetails->setCreatedAt(new \DateTime());
+
+            $this->paymentDetailsRepository->getEntityManager()->persist($paymentDetails);
+        }
+
+        $this->paymentDetailsRepository->getEntityManager()->flush();
+    }
+
+    /**
+     * @Then I will see :arg1 payment details
+     */
+    public function iWillSeePaymentDetails($arg1)
+    {
+        $data = $this->getJsonContent();
+
+        if (!isset($data['payment_details'])) {
+            throw new \Exception('No payment details found');
+        }
+
+        if (count($data['payment_details']) != $arg1) {
+            throw new \Exception('Wrong count');
+        }
+    }
+}
