8886485eb41b52b101475a26afbe9ac52e4b73dc
Iain <iain@humblyarrogant.io>
2023-05-04 14:52:04 +0200

    Stripe import command

---
+++ src/Command/StripeImportCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Command;
+
+use App\Entity\StripeImport;
+use App\Import\Stripe\CustomerImporter;
+use App\Import\Stripe\ProductImporter;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'billabear:stripe:import', description: 'Import product data from stripe')]
+class StripeImportCommand extends Command
+{
+    public function __construct(
+        private CustomerImporter $customerImporter,
+        private ProductImporter $productImporter,
+    ) {
+        parent::__construct(null);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Start stripe import command');
+        $import = new StripeImport();
+        $this->customerImporter->import($import, false);
+        $output->writeln('Start stripe product import command');
+        $this->productImporter->import($import, false);
+
+        return Command::SUCCESS;
+    }
+}
