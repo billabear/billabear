24092ea09de17e64b16165fa48fea8fa95db1bc1
Iain <iain@humblyarrogant.io>
2023-05-09 09:53:04 +0200

    Subscription creation process WIP

---
+++ src/EventSubscriber/SubscriptionSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\EventSubscriber;
+
+use App\Entity\SubscriptionCreation;
+use App\Repository\SubscriptionCreationRepositoryInterface;
+use App\Subscription\SubscriptionCreationProcessor;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class SubscriptionSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private SubscriptionCreationRepositoryInterface $subscriptionCreationRepository,
+        private SubscriptionCreationProcessor $subscriptionCreationProcessor,
+    ) {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            SubscriptionCreation::NAME => [
+                'handleNewSubscription',
+            ],
+        ];
+    }
+
+    public function handleNewSubscription(SubscriptionCreation $subscriptionCreation): void
+    {
+        $subscriptionCreation = new SubscriptionCreation();
+        $subscriptionCreation->setState('started');
+        $subscriptionCreation->setSubscription($subscriptionCreation->getSubscription());
+        $subscriptionCreation->setCreatedAt(new \DateTime());
+
+        $this->subscriptionCreationRepository->save($subscriptionCreation);
+        $this->subscriptionCreationProcessor->process($subscriptionCreation);
+    }
+}
---
+++ src/Subscription/SubscriptionCreationProcessor.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Subscription;
+
+use App\Entity\SubscriptionCreation;
+use App\Repository\SubscriptionCreationRepositoryInterface;
+use Parthenon\Common\LoggerAwareTrait;
+use Symfony\Component\Workflow\WorkflowInterface;
+
+class SubscriptionCreationProcessor
+{
+    use LoggerAwareTrait;
+
+    public const TRANSITIONS = ['handle_stats', 'send_customer_notice', 'send_internal_notice'];
+
+    public function __construct(
+        private WorkflowInterface $subscriptionCreationStateMachine,
+        private SubscriptionCreationRepositoryInterface $subscriptionCreationRepository,
+    ) {
+    }
+
+    public function process(SubscriptionCreation $request): void
+    {
+        $atateMachine = $this->subscriptionCreationStateMachine;
+
+        try {
+            foreach (self::TRANSITIONS as $transition) {
+                if ($atateMachine->can($request, $transition)) {
+                    $atateMachine->apply($request, $transition);
+
+                    $this->getLogger()->info('Did subscription creation transition', ['transition' => $transition]);
+                } else {
+                    $this->getLogger()->info("Can't do subscription creation transition", ['transition' => $transition]);
+                }
+            }
+        } catch (\Throwable $e) {
+            $this->getLogger()->info('Subscription Creation transition failed', ['transition' => $transition, 'message' => $e->getMessage()]);
+            $request->setError($e->getMessage());
+        }
+
+        $this->subscriptionCreationRepository->save($request);
+    }
+}
