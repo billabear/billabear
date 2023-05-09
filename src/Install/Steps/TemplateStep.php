ae1cb2f812ea53ba8711e7601f7a1a5575c9ef40
Iain <iain@humblyarrogant.io>
2023-05-09 17:39:42 +0200

    Handle subscription creation and cancel templates

--- src/Install/Steps/TemplateStep.php
+++ src/Install/Steps/TemplateStep.php
@@ -42,7 +42,31 @@ class TemplateStep
         $emailTemplate = new EmailTemplate();
         $emailTemplate->setName(EmailTemplate::NAME_SUBSCRIPTION_CREATED);
         $emailTemplate->setSubject('Subscription Created');
-        $emailTemplate->setTemplateBody('Subscription Created');
+        $emailTemplate->setTemplateBody('<html>
+    <head>
+      <title></title>
+    </head>
+    <body style="background: rgb(254,234,0);
+background: radial-gradient(circle, rgba(254,234,0,1) 0%, rgba(246,156,0,1) 100%);; color: black;">
+    
+    <div style="padding-top: 40px;">
+      <div style="margin:auto; background-color: white; max-width: 700px; padding: 50px; border-radius: 15px; margin-top: 40px; ">
+        <h1 style="text-align:center;"><img src="https://ha-static-data.s3.eu-central-1.amazonaws.com/github-readme-logo.png" alt="{{ brand.name  }}" /></h1>
+        
+        <p>Your subscription for plan <strong>{{ subscription.plan_name }}</strong> has been started</p>
+        
+        {% if subscription.has_trial %}
+            <p>You have a trial which will last {{ subscription.trial_length  }} after which you will be charged {{ subscription.amount }}</p>
+        {% endif %}
+      
+        <p>If you have any questions just reach out and we\'ll be happy to answer them!</p>
+      </div>
+
+      </div>
+
+
+    </body>
+  </html>');
         $emailTemplate->setBrand($brand);
         $emailTemplate->setUseEmspTemplate(false);
         $emailTemplate->setLocale(Customer::DEFAULT_LOCALE);
@@ -78,7 +102,28 @@ class TemplateStep
         $emailTemplate = new EmailTemplate();
         $emailTemplate->setName(EmailTemplate::NAME_SUBSCRIPTION_CANCELLED);
         $emailTemplate->setSubject('Subscription Cancelled');
-        $emailTemplate->setTemplateBody('Your subscription has been cancelled');
+        $emailTemplate->setTemplateBody('<html>
+    <head>
+      <title></title>
+    </head>
+    <body style="background: rgb(254,234,0);
+background: radial-gradient(circle, rgba(254,234,0,1) 0%, rgba(246,156,0,1) 100%);; color: black;">
+    
+    <div style="padding-top: 40px;">
+      <div style="margin:auto; background-color: white; max-width: 700px; padding: 50px; border-radius: 15px; margin-top: 40px; ">
+        <h1 style="text-align:center;"><img src="https://ha-static-data.s3.eu-central-1.amazonaws.com/github-readme-logo.png" alt="{{ brand.name  }}" /></h1>
+        
+        <p>Your subscription for plan <strong>{{ subscription.plan_name }}</strong> has been cancelled</p>
+        <p>You will stop being able to use the system at {{ subscription.finishes_at }}</p>
+      
+        <p>If you have any questions just reach out and we\'ll be happy to answer them!</p>
+      </div>
+
+      </div>
+
+
+    </body>
+  </html>');
         $emailTemplate->setBrand($brand);
         $emailTemplate->setUseEmspTemplate(false);
         $emailTemplate->setLocale(Customer::DEFAULT_LOCALE);
