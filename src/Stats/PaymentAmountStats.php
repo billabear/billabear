b5e1ce04cc7c0313396743ea39d57764272803ef
Iain <iain@humblyarrogant.io>
2023-05-10 16:15:04 +0200

    Add payment stats processor

---
+++ src/Stats/PaymentAmountStats.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Stats;
+
+use App\Repository\Stats\PaymentAmountDailyStatsRepositoryInterface;
+use App\Repository\Stats\PaymentAmountMonthlyStatsRepositoryInterface;
+use App\Repository\Stats\PaymentAmountYearlyStatsRepositoryInterface;
+use Parthenon\Billing\Entity\Payment;
+
+class PaymentAmountStats
+{
+    public function __construct(
+        private PaymentAmountDailyStatsRepositoryInterface $dailyStatsRepository,
+        private PaymentAmountMonthlyStatsRepositoryInterface $monthlyStatsRepository,
+        private PaymentAmountYearlyStatsRepositoryInterface $yearlyStatsRepository,
+    ) {
+    }
+
+    public function process(Payment $payment): void
+    {
+        $dailyStat = $this->dailyStatsRepository->getStatForDateTimeAndCurrency($payment->getCreatedAt(), $payment->getMoneyAmount()->getCurrency());
+        $dailyStat->increaseAmount($payment->getMoneyAmount());
+        $this->dailyStatsRepository->save($dailyStat);
+
+        $monthlyStat = $this->monthlyStatsRepository->getStatForDateTimeAndCurrency($payment->getCreatedAt(), $payment->getMoneyAmount()->getCurrency());
+        $monthlyStat->increaseAmount($payment->getMoneyAmount());
+        $this->dailyStatsRepository->save($monthlyStat);
+
+        $yearlyStat = $this->yearlyStatsRepository->getStatForDateTimeAndCurrency($payment->getCreatedAt(), $payment->getMoneyAmount()->getCurrency());
+        $yearlyStat->increaseAmount($payment->getMoneyAmount());
+        $this->yearlyStatsRepository->save($yearlyStat);
+    }
+}
