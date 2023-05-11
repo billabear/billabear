61d9a010717a37418878ca9374eda442782e7391
Iain <iain@humblyarrogant.io>
2023-05-11 14:43:38 +0200

    Add charge back creation proceso transition

---
+++ src/Stats/ChargeBackAmountStats.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Stats;
+
+use App\Entity\Customer;
+use App\Repository\Stats\ChargeBackAmountDailyStatsRepositoryInterface;
+use App\Repository\Stats\ChargeBackAmountMonthlyStatsRepositoryInterface;
+use App\Repository\Stats\ChargeBackAmountYearlyStatsRepositoryInterface;
+use Parthenon\Billing\Entity\ChargeBack;
+
+class ChargeBackAmountStats
+{
+    public function __construct(
+        private ChargeBackAmountDailyStatsRepositoryInterface $dailyStatsRepository,
+        private ChargeBackAmountMonthlyStatsRepositoryInterface $monthlyStatsRepository,
+        private ChargeBackAmountYearlyStatsRepositoryInterface $yearlyStatsRepository,
+    ) {
+    }
+
+    public function process(ChargeBack $chargeBack): void
+    {
+        $payment = $chargeBack->getPayment();
+        /** @var Customer $customer */
+        $customer = $payment->getCustomer();
+        $brand = $customer?->getBrand() ?? Customer::DEFAULT_BRAND;
+
+        $dailyStat = $this->dailyStatsRepository->getStatForDateTimeAndCurrency($payment->getCreatedAt(), $payment->getMoneyAmount()->getCurrency(), $brand);
+        $dailyStat->increaseAmount($payment->getMoneyAmount());
+        $this->dailyStatsRepository->save($dailyStat);
+
+        $monthlyStat = $this->monthlyStatsRepository->getStatForDateTimeAndCurrency($payment->getCreatedAt(), $payment->getMoneyAmount()->getCurrency(), $brand);
+        $monthlyStat->increaseAmount($payment->getMoneyAmount());
+        $this->dailyStatsRepository->save($monthlyStat);
+
+        $yearlyStat = $this->yearlyStatsRepository->getStatForDateTimeAndCurrency($payment->getCreatedAt(), $payment->getMoneyAmount()->getCurrency(), $brand);
+        $yearlyStat->increaseAmount($payment->getMoneyAmount());
+        $this->yearlyStatsRepository->save($yearlyStat);
+    }
+}
---
+++ src/Workflow/ChargeBackCreation/HandleStatsTransition.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Workflow\ChargeBackCreation;
+
+use App\Entity\ChargeBackCreation;
+use App\Stats\ChargeBackAmountStats;
+use Parthenon\Common\LoggerAwareTrait;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Workflow\Event\Event;
+
+class HandleStatsTransition implements EventSubscriberInterface
+{
+    use LoggerAwareTrait;
+
+    public function __construct(
+        private ChargeBackAmountStats $amountStats,
+    ) {
+    }
+
+    public function transition(Event $event)
+    {
+        $chargeBackCreation = $event->getSubject();
+
+        if (!$chargeBackCreation instanceof ChargeBackCreation) {
+            $this->getLogger()->error('Refund creation transition has something other than a PaymentCreated object', ['class' => get_class($chargeBackCreation)]);
+
+            return;
+        }
+        $this->amountStats->process($chargeBackCreation->getChargeBack());
+
+        $this->getLogger()->info('Payment stats generated');
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'workflow.charge_back_creation.transition.handle_stats' => ['transition'],
+        ];
+    }
+}
