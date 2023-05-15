f9dad090a0668127d1ce9742525054bd7174cfb0
Iain <iain@humblyarrogant.io>
2023-05-15 13:11:57 +0200

    Add day before command

---
+++ src/Command/ExpiringCardsDayBeforeommand.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Command;
+
+use App\Background\ExpiringCards\StartProcess;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'billabear:expiring-cards:day-before', description: 'Handle the day before next charge check')]
+class ExpiringCardsDayBeforeommand extends Command
+{
+    public function __construct(private StartProcess $startProcess)
+    {
+        parent::__construct(null);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Start checking the expiring cards if they are to be charge within the next 24 hours');
+        $this->startProcess->execute();
+
+        return Command::SUCCESS;
+    }
+}
