a1e6a4f118d98837c572750db9209cdc82bb077b
Iain <iain@humblyarrogant.io>
2023-05-24 20:49:43 +0200

    Add invoice pdf generator

---
+++ src/Pdf/InvoicePdfGenerator.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Pdf;
+
+use App\Entity\Customer;
+use App\Entity\Invoice;
+use App\Entity\Template;
+use App\Repository\TemplateRepositoryInterface;
+use Parthenon\Common\Exception\NoEntityFoundException;
+use Parthenon\Common\Pdf\GeneratorInterface;
+use Twig\Environment;
+
+class InvoicePdfGenerator
+{
+    public function __construct(
+        private TemplateRepositoryInterface $templateRepository,
+        private Environment $twig,
+        private GeneratorInterface $pdfGenerator,
+    ) {
+    }
+
+    public function generate(Invoice $invoice)
+    {
+        $customer = $invoice->getCustomer();
+
+        if (!$customer instanceof Customer) {
+            throw new \LogicException('Invalid customer type');
+        }
+        try {
+            $template = $this->templateRepository->getByNameAndBrand(Template::NAME_INVOICE, $customer->getBrand());
+        } catch (NoEntityFoundException $exception) {
+            $template = $this->templateRepository->getByNameAndBrand(Template::NAME_INVOICE, Customer::DEFAULT_BRAND);
+        }
+
+        $twigTemplate = $this->twig->createTemplate($template->getContent());
+        $content = $this->twig->render($twigTemplate, ['invoice' => $invoice, 'customer' => $invoice->getCustomer()]);
+
+        return $this->pdfGenerator->generate($content);
+    }
+}
