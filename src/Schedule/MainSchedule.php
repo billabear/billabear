1e62190066c6acea10e664b4ae403eff48bbe3c1
Iain <iain@humblyarrogant.io>
2023-05-27 22:52:28 +0200

    Add scheduler basics

---
+++ src/Schedule/MainSchedule.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Schedule;
+
+use App\Messenger\Message\StripeImport;
+use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\RecurringMessage;
+use Symfony\Component\Scheduler\Schedule;
+use Symfony\Component\Scheduler\ScheduleProviderInterface;
+
+#[AsSchedule('main')]
+class MainSchedule implements ScheduleProviderInterface
+{
+    public function getSchedule(): Schedule
+    {
+        $schedule = new Schedule();
+        $schedule->add(RecurringMessage::every('1 minute', new StripeImport()));
+
+        return $schedule;
+    }
+}
