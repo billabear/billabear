72bdb180fc2801c40462d7f5bb775534e20a98f3
Iain <iain@humblyarrogant.io>
2023-05-31 14:12:24 +0200

    CachedStatsRepository

---
+++ src/Entity/Stats/CachedStats.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Entity\Stats;
+
+use App\Enum\CachedStatsType;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+
+#[ORM\Entity()]
+#[ORM\Table('stats_cached_stats')]
+class CachedStats
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private $id;
+
+    #[ORM\Column(type: 'string', enumType: CachedStatsType::class)]
+    private CachedStatsType $type;
+
+    #[ORM\Column(type: 'string', unique: true)]
+    private string $name;
+
+    #[ORM\Column(type: 'integer')]
+    private int $value;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $currency;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): void
+    {
+        $this->value = $value;
+    }
+
+    public function getCurrency(): ?string
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(?string $currency): void
+    {
+        $this->currency = $currency;
+    }
+}
