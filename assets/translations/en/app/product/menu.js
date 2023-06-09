e8dd9e26f2e2aa44497811823a740ce2c5af90da
Iain <iain@humblyarrogant.io>
2023-06-09 23:46:20 +0200

    Update product menu

--- assets/translations/en/app/product/menu.js
+++ assets/translations/en/app/product/menu.js
@@ -1,4 +1,5 @@
 export const PRODUCT_MENU_TRANSLATIONS = {
+    title: "Product",
     products: "Products",
     features: "Features",
     vouchers: "Vouchers"
--- assets/views/App/Product/ProductGroup.vue
+++ assets/views/App/Product/ProductGroup.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex">
     <div class="w-1/5">
-      <div class="mt-5 rounded-lg p-5 border shadow-lg">
+      <div class="submenu-container">
+        <div class="submenu-title">{{ $t('app.product.menu.title') }}</div>
         <ul>
-          <li class="my-3"><router-link :to="{name: 'app.product.list'}">{{ $t('app.product.menu.products') }}</router-link></li>
-          <li class="my-3"><router-link :to="{name: 'app.feature.list'}">{{ $t('app.product.menu.features') }}</router-link></li>
-          <li class="my-3"><router-link :to="{name: 'app.vouchers.list'}">{{ $t('app.product.menu.vouchers') }}</router-link></li>
+          <li class="submenu-list-item"><router-link :to="{name: 'app.product.list'}" class="submenu-link"><i class="fa-solid fa-box"></i> {{ $t('app.product.menu.products') }}</router-link></li>
+          <li class="submenu-list-item"><router-link :to="{name: 'app.feature.list'}" class="submenu-link"><i class="fa-sharp fa-solid fa-gear"></i> {{ $t('app.product.menu.features') }}</router-link></li>
+          <li class="submenu-list-item"><router-link :to="{name: 'app.vouchers.list'}" class="submenu-link"><i class="fa-solid fa-gift"></i> {{ $t('app.product.menu.vouchers') }}</router-link></li>
         </ul>
       </div>
     </div>
