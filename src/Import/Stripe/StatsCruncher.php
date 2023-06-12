e85b44909cd9b458a90d4a68f30e64399005122c
Iain <iain@humblyarrogant.io>
2023-06-12 17:18:36 +0200

    Add customer creation stats to stripe import stats crunch

--- src/Import/Stripe/StatsCruncher.php
+++ src/Import/Stripe/StatsCruncher.php
@@ -12,17 +12,23 @@
 
 namespace App\Import\Stripe;
 
+use App\Stats\CreateSubscriptionCountStats;
+use App\Stats\CustomerCreationStats;
 use App\Stats\RevenueEstimatesGeneration;
 
 class StatsCruncher
 {
     public function __construct(
-        private RevenueEstimatesGeneration $estimatesGeneration
+        private RevenueEstimatesGeneration $estimatesGeneration,
+        private CreateSubscriptionCountStats $subscriptionCountStats,
+        private CustomerCreationStats $customerCreationStats,
     ) {
     }
 
     public function execute()
     {
         $this->estimatesGeneration->generate();
+        $this->subscriptionCountStats->generate();
+        $this->customerCreationStats->generate();
     }
 }
