e07af823b7d3e9d8bf9866d1cdd8ff452fa9bf34
Iain <iain@humblyarrogant.io>
2023-06-12 16:52:11 +0200

    Customer creation stats entities and repositories

---
+++ src/Repository/Stats/CustomerCreationDailyStatsRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Copyright Humbly Arrogant Software Limited 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Repository\Stats;
+
+use App\Entity\Stats\CustomerCreationDailyStats;
+
+interface CustomerCreationDailyStatsRepositoryInterface extends AmountRepositoryInterface
+{
+    public function getStatForDateTime(\DateTimeInterface $dateTime, string $brandCode): CustomerCreationDailyStats;
+}
