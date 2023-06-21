39ac4ae9f26baca2b1e7361d0da26e61950a4d94
Iain <iain@humblyarrogant.io>
2023-06-21 20:06:10 +0200

    Prepare for release

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,7 +5,7 @@
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  *
- * Change Date: TBD ( 3 years after 1.0.0 release )
+ * Change Date: 26.06.2026 ( 3 years after 1.0.0 release )
  *
  * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
  */
@@ -18,6 +18,6 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const VERSION = '1.0-DEV';
+    public const VERSION = '1.0';
     public const VERSION_ID = '100000';
 }
