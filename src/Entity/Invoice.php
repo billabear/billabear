45bcc3fd56260a5d1603f1096f47482534a124ee
Iain <iain@humblyarrogant.io>
2023-06-28 12:57:10 +0200

    Update copyright header to prepare for v1.1

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  *
- * Change Date: 26.06.2026 ( 3 years after 1.0.0 release )
+ * Change Date: DD.MM.2026 ( 3 years after 1.1.0 release )
  *
  * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
  */
--- tests/Behat/Settings/NotificationSettingsContext.php
+++ tests/Behat/Settings/NotificationSettingsContext.php
@@ -5,14 +5,13 @@
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  *
- * Change Date: 26.06.2026 ( 3 years after 1.0.0 release )
+ * Change Date: DD.MM.2026 ( 3 years after 1.1.0 release )
  *
  * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
  */
 
 namespace App\Tests\Behat\Settings;
 
-use App\Entity\Settings;
 use App\Repository\Orm\SettingsRepository;
 use App\Tests\Behat\SendRequestTrait;
 use Behat\Behat\Context\Context;
