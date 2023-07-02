227c1439c869f6ae0a3d95a2ac23dc0ad99e4a5a
Iain <iain@humblyarrogant.io>
2023-07-02 16:47:41 +0200

    Put in dev

--- src/Kernel.php
+++ src/Kernel.php
@@ -18,6 +18,6 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public const VERSION = '1.0.1';
-    public const VERSION_ID = '100010';
+    public const VERSION = '1.0-dev';
+    public const VERSION_ID = '100000-DEV';
 }
