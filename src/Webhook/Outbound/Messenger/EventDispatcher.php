b60bb376f5e588ece74d094807cdbdcc5d974f2d
Iain <iain@humblyarrogant.io>
2023-08-01 00:50:14 +0200

    Add webhook dispatch to background

---
+++ src/Webhook/Outbound/Messenger/EventDispatcher.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Copyright Humbly Arrogant Software Limited 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: DD.MM.2026 ( 3 years after 1.1.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Webhook\Outbound\Messenger;
+
+use App\Webhook\Outbound\EventDispatcherInterface;
+use App\Webhook\Outbound\Payload\PayloadInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+    public function __construct(private MessageBusInterface $messageBus)
+    {
+    }
+
+    public function dispatch(PayloadInterface $payload): void
+    {
+        $this->messageBus->dispatch(new EventMessage($payload->getType(), $payload->getPayload()));
+    }
+}
---
+++ src/Webhook/Outbound/Messenger/EventMessage.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Copyright Humbly Arrogant Software Limited 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ *
+ * Change Date: DD.MM.2026 ( 3 years after 1.1.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace App\Webhook\Outbound\Messenger;
+
+use App\Enum\WebhookEventType;
+use App\Webhook\Outbound\Payload\PayloadInterface;
+
+class EventMessage implements PayloadInterface
+{
+    public function __construct(public WebhookEventType $type, private array $payload)
+    {
+    }
+
+    public function getType(): WebhookEventType
+    {
+        return $this->type;
+    }
+
+    public function getPayload(): array
+    {
+        return $this->payload;
+    }
+}
