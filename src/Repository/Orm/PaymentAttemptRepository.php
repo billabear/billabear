12f615a18462f38ffef7118a1060add8134d756e
Iain <iain@humblyarrogant.io>
2023-08-23 20:21:02 +0200

    Update copyright header to prepare for v1.1

--- src/Repository/Orm/PaymentAttemptRepository.php
+++ src/Repository/Orm/PaymentAttemptRepository.php
@@ -5,7 +5,7 @@
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  *
- * Change Date: DD.MM.2026 ( 3 years after 1.1.0 release )
+ * Change Date: 24.08.2026 ( 3 years after 1.1.0 release )
  *
  * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
  */
