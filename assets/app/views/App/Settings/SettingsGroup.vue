5e1c55fcbd1bdaa62f143e1d468dc21f1ffde6ca
Iain <iain@humblyarrogant.io>
2023-09-28 16:27:27 +0200

    Move the templates

--- assets/app/views/App/Settings/SettingsGroup.vue
+++ assets/app/views/App/Settings/SettingsGroup.vue
@@ -3,6 +3,12 @@
     <div class="">
       <div class="submenu-container">
         <ul>
+          <RoleOnlyView role="ROLE_ACCOUNT_MANAGER">
+            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.pdf_template.list'}" class="submenu-link">{{ $t('app.settings.menu.pdf_templates') }}</router-link></li>
+            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.email_template.list'}" class="submenu-link">{{ $t('app.settings.menu.email_templates') }}</router-link></li>
+            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.brand_settings.list'}" class="submenu-link">{{ $t('app.settings.menu.brand_settings') }}</router-link></li>
+            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.exchange_rates.list'}" class="submenu-link">{{ $t('app.settings.menu.exchange_rates') }}</router-link></li>
+          </RoleOnlyView>
          <RoleOnlyView role="ROLE_ADMIN">
             <li class="submenu-list-item"><router-link :to="{name: 'app.settings.tax_settings.update'}" class="submenu-link">{{ $t('app.settings.menu.tax_settings') }}</router-link></li>
             <li class="submenu-list-item"><router-link :to="{name: 'app.settings.import.stripe'}" class="submenu-link">{{ $t('app.settings.menu.stripe') }}</router-link></li>
@@ -12,12 +18,6 @@
             <li class="submenu-list-item"><router-link :to="{name: 'app.settings.notification_settings.update'}" class="submenu-link">{{ $t('app.settings.menu.notification_settings') }}</router-link></li>
             <li class="submenu-list-item"><router-link :to="{name: 'app.settings.system_settings.update'}" class="submenu-link">{{ $t('app.settings.menu.system_settings') }}</router-link></li>
             <li class="submenu-list-item"><router-link :to="{name: 'app.settings.api_keys.main'}" class="submenu-link">{{ $t('app.settings.menu.api_keys') }}</router-link></li>
-          </RoleOnlyView>
-          <RoleOnlyView role="ROLE_ACCOUNT_MANAGER">
-            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.pdf_template.list'}" class="submenu-link">{{ $t('app.settings.menu.pdf_templates') }}</router-link></li>
-            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.email_template.list'}" class="submenu-link">{{ $t('app.settings.menu.email_templates') }}</router-link></li>
-            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.brand_settings.list'}" class="submenu-link">{{ $t('app.settings.menu.brand_settings') }}</router-link></li>
-            <li class="submenu-list-item"><router-link :to="{name: 'app.settings.exchange_rates.list'}" class="submenu-link">{{ $t('app.settings.menu.exchange_rates') }}</router-link></li>
           </RoleOnlyView>
         </ul>
       </div>
