22ca36a4a6d6a444d3ac01c158ebf9111dbd285e
Iain <iain@humblyarrogant.io>
2023-10-08 17:29:10 +0200

    Translation changes

--- assets/app/views/App/Workflows/CancellationRequest/CancellationRequestView.vue
+++ assets/app/views/App/Workflows/CancellationRequest/CancellationRequestView.vue
@@ -1,56 +1,56 @@
 <template>
   <div>
-    <h1 class="page-title">{{ $t('app.system.cancellation_request.view.title') }}</h1>
+    <h1 class="page-title">{{ $t('app.workflows.cancellation_request.view.title') }}</h1>
 
     <LoadingScreen :ready="ready">
       <div class="mx-5 grid grid-cols-2 gap-5">
 
         <div class="card-body">
-          <div class="section-header">{{ $t('app.system.cancellation_request.view.subscription.title') }}</div>
+          <div class="section-header">{{ $t('app.workflows.cancellation_request.view.subscription.title') }}</div>
           <dl class="detail-list">
             <div>
-              <dt>{{ $t('app.system.cancellation_request.view.subscription.name') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.subscription.name') }}</dt>
               <dd>{{ cancellation_request.subscription.plan.name }}</dd>
             </div>
             <div>
-              <dt>{{ $t('app.system.cancellation_request.view.subscription.customer') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.subscription.customer') }}</dt>
               <dd><router-link :to="{name: 'app.customer.view', params: {id: cancellation_request.subscription.customer.id}}">{{ cancellation_request.subscription.customer.email }}</router-link></dd>
             </div>
             <div>
-              <dt>{{ $t('app.system.cancellation_request.view.subscription.original_cancellation_date') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.subscription.original_cancellation_date') }}</dt>
               <dd>{{ $filters.moment(cancellation_request.original_valid_until, 'LL') }}</dd>
             </div>
           </dl>
         </div>
 
         <div class="card-body">
-          <div class="section-header">{{ $t('app.system.cancellation_request.view.details.title') }}</div>
+          <div class="section-header">{{ $t('app.workflows.cancellation_request.view.details.title') }}</div>
           <dl class="detail-list">
             <div>
-              <dt>{{ $t('app.system.cancellation_request.view.details.state') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.details.state') }}</dt>
               <dd>{{ cancellation_request.state }}</dd>
             </div>
             <div>
-              <dt>{{ $t('app.system.cancellation_request.view.details.when') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.details.when') }}</dt>
               <dd>{{ cancellation_request.when }}</dd>
             </div>
             <div v-if="cancellation_request.when == 'specific-date'">
-              <dt>{{ $t('app.system.cancellation_request.view.details.specific_date') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.details.specific_date') }}</dt>
               <dd>{{ $filters.moment(cancellation_request.specific_date, 'LL')}}</dd>
             </div>
             <div>
-              <dt>{{ $t('app.system.cancellation_request.view.details.refund_type') }}</dt>
+              <dt>{{ $t('app.workflows.cancellation_request.view.details.refund_type') }}</dt>
               <dd>{{ cancellation_request.refund_type }}</dd>
             </div>
           </dl>
         </div>
       </div>
       <div class="card-body m-5" v-if="cancellation_request.has_error">
-        <div class="section-header">{{ $t('app.system.cancellation_request.view.error.title') }}</div>
+        <div class="section-header">{{ $t('app.workflows.cancellation_request.view.error.title') }}</div>
         <pre>{{ cancellation_request.error }}</pre>
       </div>
       <div class="m-5">
-        <SubmitButton :in-progress="sending" class="btn--secondary" @click="callProcess" v-if="cancellation_request.state !== 'completed'">{{ $t('app.system.cancellation_request.view.buttons.process') }}</SubmitButton>
+        <SubmitButton :in-progress="sending" class="btn--secondary" @click="callProcess" v-if="cancellation_request.state !== 'completed'">{{ $t('app.workflows.cancellation_request.view.buttons.process') }}</SubmitButton>
       </div>
     </LoadingScreen>
   </div>
