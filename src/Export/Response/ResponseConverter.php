763c054d081c13fb812e5a6bc85a97449cfd1d73
Iain <iain@humblyarrogant.io>
2023-10-09 17:09:25 +0200

    Change header

--- src/Export/Response/ResponseConverter.php
+++ src/Export/Response/ResponseConverter.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  *
- * Change Date: 24.08.2026 ( 3 years after 1.1.0 release )
+ * Change Date: xx.10.2026 ( 3 years after 2023.4 release )
  *
  * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
  */
