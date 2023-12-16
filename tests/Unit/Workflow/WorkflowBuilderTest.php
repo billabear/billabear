ccc759b892c4d98d63b478711ae122da1de23460
Iain <iain@humblyarrogant.io>
2023-12-16 12:03:26 +0100

    Add tests for workflow tracable

--- tests/Unit/Workflow/WorkflowBuilderTest.php
+++ tests/Unit/Workflow/WorkflowBuilderTest.php
@@ -21,6 +21,7 @@ use App\Workflow\TransitionHandlers\DynamicHandlerProvider;
 use App\Workflow\WorkflowBuilder;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\Workflow\Debug\TraceableWorkflow;
 
 class WorkflowBuilderTest extends TestCase
 {
@@ -176,5 +177,121 @@ class WorkflowBuilderTest extends TestCase
         $this->assertEquals([$placeName], $transition->getFroms());
         $this->assertEquals([$placeClassName], $transition->getTos());
         $this->assertEquals($placeClassTransition, $transition->getName());
+    }
+
+    public function testThatItReturnsATracableWorkflowIfTest(): void
+    {
+        $handlerName = 'cool_handler';
+        $placeName = 'a_cool_place';
+        $transitionName = 'transition_a_cool_place';
+
+        $placesProvider = $this->createMock(PlacesProvider::class);
+        $eventDispatcher = $this->createMock(EventDispatcher::class);
+        $dynamicHandlerProvider = $this->createMock(DynamicHandlerProvider::class);
+        $handler = $this->createMock(DynamicHandlerInterface::class);
+        $placeEntity = $this->createMock(WorkflowTransition::class);
+
+        $placeEntity->method('getHandlerName')->willReturn($handlerName);
+        $placeEntity->method('getToTransitionName')->willReturn($transitionName);
+        $placeEntity->method('getName')->willReturn($placeName);
+
+        $placeClassName = 'hardcoded_place';
+        $placeClassTransition = 'hardcoded_transition';
+
+        $placeClass = new class($placeClassName, $placeClassTransition) implements PlaceInterface {
+            public function __construct(private string $placeClassName, private string $placeClassTransition)
+            {
+            }
+
+            public function getName(): string
+            {
+                return $this->placeClassName;
+            }
+
+            public function getPriority(): int
+            {
+                return 100;
+            }
+
+            public function getWorkflow(): WorkflowType
+            {
+                return WorkflowType::CANCEL_SUBSCRIPTION;
+            }
+
+            public function getToTransitionName(): string
+            {
+                return $this->placeClassTransition;
+            }
+        };
+
+        $placesProvider->method('getPlacesForWorkflow')->with(WorkflowType::CANCEL_SUBSCRIPTION)->willReturn([$placeEntity, $placeClass]);
+        $dynamicHandlerProvider->method('getHandlerByName')->with($handlerName)->willReturn($handler);
+
+        $subject = new WorkflowBuilder(
+            $placesProvider,
+            $eventDispatcher,
+            $dynamicHandlerProvider,
+            'test'
+        );
+        $workflow = $subject->build(WorkflowType::CANCEL_SUBSCRIPTION);
+        $this->assertInstanceOf(TraceableWorkflow::class, $workflow);
+    }
+
+    public function testThatItDoesNotReturnsATracableWorkflowIfProd(): void
+    {
+        $handlerName = 'cool_handler';
+        $placeName = 'a_cool_place';
+        $transitionName = 'transition_a_cool_place';
+
+        $placesProvider = $this->createMock(PlacesProvider::class);
+        $eventDispatcher = $this->createMock(EventDispatcher::class);
+        $dynamicHandlerProvider = $this->createMock(DynamicHandlerProvider::class);
+        $handler = $this->createMock(DynamicHandlerInterface::class);
+        $placeEntity = $this->createMock(WorkflowTransition::class);
+
+        $placeEntity->method('getHandlerName')->willReturn($handlerName);
+        $placeEntity->method('getToTransitionName')->willReturn($transitionName);
+        $placeEntity->method('getName')->willReturn($placeName);
+
+        $placeClassName = 'hardcoded_place';
+        $placeClassTransition = 'hardcoded_transition';
+
+        $placeClass = new class($placeClassName, $placeClassTransition) implements PlaceInterface {
+            public function __construct(private string $placeClassName, private string $placeClassTransition)
+            {
+            }
+
+            public function getName(): string
+            {
+                return $this->placeClassName;
+            }
+
+            public function getPriority(): int
+            {
+                return 100;
+            }
+
+            public function getWorkflow(): WorkflowType
+            {
+                return WorkflowType::CANCEL_SUBSCRIPTION;
+            }
+
+            public function getToTransitionName(): string
+            {
+                return $this->placeClassTransition;
+            }
+        };
+
+        $placesProvider->method('getPlacesForWorkflow')->with(WorkflowType::CANCEL_SUBSCRIPTION)->willReturn([$placeEntity, $placeClass]);
+        $dynamicHandlerProvider->method('getHandlerByName')->with($handlerName)->willReturn($handler);
+
+        $subject = new WorkflowBuilder(
+            $placesProvider,
+            $eventDispatcher,
+            $dynamicHandlerProvider,
+            'prod'
+        );
+        $workflow = $subject->build(WorkflowType::CANCEL_SUBSCRIPTION);
+        $this->assertNotInstanceOf(TraceableWorkflow::class, $workflow);
     }
 }
