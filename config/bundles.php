64594b7798f5607f37d588b504539f0000930ebb
Iain <iain@humblyarrogant.io>
2024-02-22 20:01:52 +0100

    Update workflows

--- config/bundles.php
+++ config/bundles.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * Copyright Iain Cambridge 2023.
+ * Copyright Humbly Arrogant Software Limited 2022-2023.
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  *
- * Change Date: TBD ( 3 years after 1.0.0 release )
+ * Change Date: DD.MM.2027 ( 3 years after 2024.1 release )
  *
  * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
  */
@@ -15,7 +15,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Parthenon\ParthenonBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
