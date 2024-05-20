603b236e792be40e2b4c8cb8af5e2a275ecc60aa
Iain <iain@humblyarrogant.io>
2024-05-21 00:34:51 +0200

    Tweak menus

--- assets/billabear/views/App/System/Integrations/Slack/SlackGroup.vue
+++ assets/billabear/views/App/System/Integrations/Slack/SlackGroup.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="grid grid-cols-6">
     <div class="border-r">
-      <span class="p-5 text-2xl font-bold">{{ $t('app.system.integrations.slack.menu.title') }}</span>
-      <ul>
-        <li><router-link :to="{name: 'app.system.integrations.slack.webhook'}">{{ $t('app.system.integrations.slack.menu.webhooks') }}</router-link></li>
-        <li><router-link :to="{name: 'app.system.integrations.slack.notification'}">{{ $t('app.system.integrations.slack.menu.notification') }}</router-link></li>
+      <span class="my-3 p-5 text-2xl font-bold">{{ $t('app.system.integrations.slack.menu.title') }}</span>
+      <ul class="mt-5">
+        <li><router-link :to="{name: 'app.system.integrations.slack.webhook'}" class="slack-menu">{{ $t('app.system.integrations.slack.menu.webhooks') }}</router-link></li>
+        <li><router-link :to="{name: 'app.system.integrations.slack.notification'}" class="slack-menu">{{ $t('app.system.integrations.slack.menu.notification') }}</router-link></li>
 
 
       </ul>
@@ -22,5 +22,7 @@ export default {
 </script>
 
 <style scoped>
-
+.slack-menu {
+  @apply px-2 text-black underline;
+}
 </style>
