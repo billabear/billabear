cf833e3d36701dafd7bfb22bb03b2a9873f48b44
Iain <iain@humblyarrogant.io>
2024-06-02 16:18:18 +0200

    Speed up code style

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,14 +1,12 @@
 <?php
 
 /*
- * Copyright Humbly Arrogant Software Limited 2022-2023.
+ * Copyright Humbly Arrogant Software Limited 2023-2024.
  *
- * Use of this software is governed by the Business Source License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
- *
- * Change Date: DD.MM.2027 ( 3 years after 2024.1 release )
- *
- * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE.md file.
+ * Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  */
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
@@ -24,4 +22,5 @@ return (new PhpCsFixer\Config())
 Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.'],
     ])
     ->setFinder($finder)
+    ->setParallelConfig(ParallelConfigFactory::detect())
 ;
