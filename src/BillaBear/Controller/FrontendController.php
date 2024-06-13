a5f3e4bb8a82095315c04c57ae280f93d80dae6d
Iain <iain@humblyarrogant.io>
2024-06-13 02:13:29 +0000

    Remove multi-tenancy from standalone app
    Automatic commit - f1bd049d2758ec96ad1df6a5fd9b3f6688d7d01a

--- src/BillaBear/Controller/FrontendController.php
+++ src/BillaBear/Controller/FrontendController.php
@@ -10,28 +10,24 @@ namespace BillaBear\Controller;
 
 use BillaBear\Repository\SettingsRepositoryInterface;
 use Doctrine\DBAL\Exception\TableNotFoundException;
-use Parthenon\MultiTenancy\Exception\NoTenantFoundException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Profiler\Profiler;
 use Symfony\Component\Routing\Attribute\Route;
 use Twig\Environment;
 
 class FrontendController
 {
-    #[Route('/', name: 'app_index_landing', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/login', name: 'app_public', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/signup', name: 'app_signup', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/signup/{code}', name: 'app_invite', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/forgot-password', name: 'app_forgot_password', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/forgot-password/{code}', name: 'app_forgot_password_confirm', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/confirm-email/{code}', name: 'app_confirm_email', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/site/{vueRouting}', name: 'app_main', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
-    #[Route('/app/plan', name: 'app_plan', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' !== '%kernel.environment%'")]
+    #[Route('/', name: 'app_index_landing', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/login', name: 'app_public', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/signup', name: 'app_signup', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/signup/{code}', name: 'app_invite', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/forgot-password', name: 'app_forgot_password', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/forgot-password/{code}', name: 'app_forgot_password_confirm', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/confirm-email/{code}', name: 'app_confirm_email', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/site/{vueRouting}', name: 'app_main', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
+    #[Route('/app/plan', name: 'app_plan', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null])]
     public function home(
-        Request $request,
         Environment $twig,
         SettingsRepositoryInterface $settingsRepository,
         LoggerInterface $logger,
@@ -43,46 +39,6 @@ class FrontendController
             $logger->info('Redirected to install page');
 
             return new RedirectResponse('/install');
-        } catch (NoTenantFoundException $e) {
-            $logger->warning('No tenant was found', ['hostname' => $request->getHost()]);
-
-            return new Response($twig->render('not_found.html.twig'));
-        }
-
-        return new Response($twig->render('index.html.twig'));
-    }
-
-    #[Route('/', name: 'app_index_landing_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/login', name: 'app_public_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/signup', name: 'app_signup_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/signup/{code}', name: 'app_invite_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/forgot-password', name: 'app_forgot_password_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/forgot-password/{code}', name: 'app_forgot_password_confirm_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/confirm-email/{code}', name: 'app_confirm_email_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/site/{vueRouting}', name: 'app_main_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    #[Route('/app/plan', name: 'app_plan_dev', requirements: ['vueRouting' => '.+'], defaults: ['vueRouting' => null], condition: "'dev' === '%kernel.environment%'")]
-    public function homeDev(
-        Request $request,
-        Environment $twig,
-        SettingsRepositoryInterface $settingsRepository,
-        LoggerInterface $logger,
-        Profiler $profiler,
-    ): Response {
-        $logger->info('A request was made to the frontend controller');
-        try {
-            $settings = $settingsRepository->getDefaultSettings();
-        } catch (TableNotFoundException $exception) {
-            $logger->info('Redirected to install page');
-
-            return new RedirectResponse('/install');
-        } catch (NoTenantFoundException $e) {
-            // Disable the profiler because if we don't we'll
-            // just get another tenant exception from the profiler in dev mode.
-            $profiler->purge();
-            $profiler->disable();
-            $logger->warning('No tenant was found', ['hostname' => $request->getHost()]);
-
-            return new Response($twig->render('not_found.html.twig'));
         }
 
         return new Response($twig->render('index.html.twig'));
