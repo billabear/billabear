9ee174db73f9126b5891c1e9dc2bbe566eb144b0
Iain <iain@humblyarrogant.io>
2024-06-27 15:58:50 +0000

    Change API output to match docs
    Automatic commit - 14ad352fc59a40292b4b2b25e6baa14a56cf545c

--- src/BillaBear/Customer/LimitsFactory.php
+++ src/BillaBear/Customer/LimitsFactory.php
@@ -8,10 +8,13 @@
 
 namespace BillaBear\Customer;
 
+use BillaBear\Dto\Generic\Api\Feature as FeatureApi;
+use BillaBear\Dto\Generic\Api\Limit as LimitApi;
 use BillaBear\Dto\Response\Api\Customer\Limits as ApiDto;
 use BillaBear\Dto\Response\App\Customer\Limits as AppDto;
 use BillaBear\Entity\Customer;
 use Parthenon\Billing\Entity\Subscription;
+use Parthenon\Billing\Entity\SubscriptionFeature;
 use Parthenon\Billing\Entity\SubscriptionPlanLimit;
 
 class LimitsFactory
@@ -21,7 +24,7 @@ class LimitsFactory
      */
     public function createApiDto(Customer $customer, array $subscriptions): ApiDto
     {
-        list($limits, $features, $userCount) = $this->getVariables($customer, $subscriptions);
+        list($limits, $features, $userCount) = $this->getApiVariables($customer, $subscriptions);
 
         $dto = new ApiDto();
         $dto->setUserCount($userCount);
@@ -75,5 +78,52 @@ class LimitsFactory
         $features = array_unique($features);
 
         return [$limits, $features, $userCount];
+    }
+
+    public function getApiVariables(Customer $customer, array $subscriptions): array
+    {
+        $limits = [];
+        $features = [];
+        $userCount = 0;
+
+        if ($customer->isDisabled()) {
+            return [$limits, $features, $userCount];
+        }
+
+        /** @var Subscription $subscription */
+        foreach ($subscriptions as $subscription) {
+            if (!$subscription->getSubscriptionPlan()) {
+                continue;
+            }
+            /** @var SubscriptionPlanLimit $limit */
+            foreach ($subscription->getSubscriptionPlan()->getLimits() as $limit) {
+                $feature = $this->buildFeature($limit->getSubscriptionFeature());
+                $name = $limit->getSubscriptionFeature()->getName();
+
+                if (!isset($limits[$name])) {
+                    $limits[$name] = new LimitApi();
+                    $limits[$name]->setFeature($feature);
+                }
+                $limits[$name]->setLimit($limits[$name]->getLimit() + $limit->getLimit());
+            }
+            foreach ($subscription->getSubscriptionPlan()->getFeatures() as $feature) {
+                $features[$feature->getCode()] = $this->buildFeature($feature);
+            }
+            $userCount += $subscription->getSubscriptionPlan()->getUserCount();
+        }
+        $limits = array_values($limits);
+        $features = array_values($features);
+
+        return [$limits, $features, $userCount];
+    }
+
+    private function buildFeature(SubscriptionFeature $feature): FeatureApi
+    {
+        $dto = new FeatureApi();
+        $dto->setName($feature->getName());
+        $dto->setDescription($feature->getDescription());
+        $dto->setCode($feature->getCode());
+
+        return $dto;
     }
 }
