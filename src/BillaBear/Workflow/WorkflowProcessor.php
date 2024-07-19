f4d8a4c0893687fd642eb0631779e99cf472d48b
Iain <iain@humblyarrogant.io>
2024-07-19 23:48:36 +0000

    Add error log to workflow processor
    Automatic commit - dd2db2286211b41c3678ba21a31e7a268dcc0b0a

--- src/BillaBear/Workflow/WorkflowProcessor.php
+++ src/BillaBear/Workflow/WorkflowProcessor.php
@@ -37,7 +37,7 @@ class WorkflowProcessor
                 }
             }
         } catch (\Throwable $e) {
-            $this->getLogger()->info('Transition for workflow failed', ['workflow' => $workflowType->value, 'transition' => $transition, 'message' => $e->getMessage()]);
+            $this->getLogger()->error('Transition for workflow failed', ['workflow' => $workflowType->value, 'transition' => $transition ?? 'unknown', 'message' => $e->getMessage()]);
             $errorMessage = sprintf("%s\n%s:%s", $e->getMessage(), $e->getFile(), $e->getLine());
 
             $subject->setError($errorMessage);
