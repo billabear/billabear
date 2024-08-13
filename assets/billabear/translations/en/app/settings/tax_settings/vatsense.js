f6bdeb13e87658781aedc709bfc1ffa1595033fd
Iain <iain@humblyarrogant.io>
2024-08-13 11:51:18 +0000

    Add translations and logo
    Automatic commit - 1c79ade377fb8b65b44e8e73c417edbd0d460688

--- assets/billabear/translations/en/app/settings/tax_settings/vatsense.js
+++ assets/billabear/translations/en/app/settings/tax_settings/vatsense.js
@@ -11,6 +11,6 @@ export const VATSENSE_TRANSLATIONS = {
         validate_vat_ids: "If you want to validate Tax ids against VAT Sense API."
     },
     description: "With our VAT Sense integration, you're able to have your tax rules automatically updated when there are changes to tax laws around the world. You're also able to have VAT IDs validated by VAT Sense so you can ensure that European customers have valid VAT IDs.",
-    create_account: "You can create a free account and upgrade later if needs are required.",
+    create_account: "You can create a free account.",
     create_account_link: "Create Account",
 }
