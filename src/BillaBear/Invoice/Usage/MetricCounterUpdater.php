caf323648c530e68e096e8bf056e487ee6db3c29
Iain <iain@humblyarrogant.io>
2024-10-15 19:22:56 +0000

    Update counter update
    Automatic commit - e50b16689020aec580f84c20d717ac1d5f1d4b1f

--- src/BillaBear/Invoice/Usage/MetricCounterUpdater.php
+++ src/BillaBear/Invoice/Usage/MetricCounterUpdater.php
@@ -44,7 +44,7 @@ class MetricCounterUpdater
             $when = $counter->getUpdatedAt();
         } else {
             // Questions should be raised about why made that nullable in the first place...
-            $when = $subscription->getStartOfCurrentPeriod() ?? new \DateTime('-61 seconds');
+            $when = $subscription->getStartOfCurrentPeriod() ?? new \DateTime('-60 seconds');
         }
 
         $metricValue = $this->metricProvider->getMetricForDateTime($subscription, $when);
