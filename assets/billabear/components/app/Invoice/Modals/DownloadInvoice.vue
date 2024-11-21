cde0e4c11eaf609892faed99563aa6d3c80a85a9
Iain <iain@humblyarrogant.io>
2024-11-21 02:00:50 +0000

    Remove console.log calls
    Automatic commit - 52a00f7f7dfc7e2a56abfce1d7904c90df07ac64

--- assets/billabear/components/app/Invoice/Modals/DownloadInvoice.vue
+++ assets/billabear/components/app/Invoice/Modals/DownloadInvoice.vue
@@ -48,11 +48,9 @@ export default {
     downloadInvoice: function () {
       this.showError = false;
       axios.get('/app/invoice/'+this.invoice.id+'/download?format='+this.format, {  responseType: 'blob'}).then(response => {
-        console.log(response);
         var fileDownload = require('js-file-download');
         const contentDisposition = response.headers['content-disposition'];
         let filename = 'invoice-'+this.invoice.id+'.pdf';
-        console.log(contentDisposition);
         if (contentDisposition) {const filenameRegex = /filename=(.*)/;
           const filenameMatch = filenameRegex.exec(contentDisposition);
           filename = filenameMatch[1];
@@ -60,7 +58,6 @@ export default {
         fileDownload(response.data, filename);
         this.downloadInProgress = false;
       }).catch(error => {
-        console.log(error);
         var that = this;
         let errorString = async function getString() {
           const str = await error.response.data.text();
