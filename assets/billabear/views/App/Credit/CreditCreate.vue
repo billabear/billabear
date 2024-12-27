b2da964792e495f24ce86ee4712f6793aa4f542e
Iain <iain@humblyarrogant.io>
2024-12-27 20:32:52 +0000

    Remove pointless css
    Automatic commit - 01799d8ef5d1021a378ea2e2adaf886a37885501

--- assets/billabear/views/App/Credit/CreditCreate.vue
+++ assets/billabear/views/App/Credit/CreditCreate.vue
@@ -110,10 +110,6 @@ export default {
   @apply block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2;
 }
 
-.form-field-input {
-  @apply appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white;
-}
-
 .form-field-help {
   @apply text-gray-600 text-xs italic;
 }
