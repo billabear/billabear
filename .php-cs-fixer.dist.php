f0fc1e2b0b497f7ade49152a870a7bc10260b975
Iain <iain@humblyarrogant.io>
2025-01-06 15:47:39 +0100

    php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright Humbly Arrogant Software Limited 2023-2024.
+ * Copyright Humbly Arrogant Software Limited 2023-2025.
  *
  * Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  */
@@ -20,6 +20,9 @@ return (new PhpCsFixer\Config())
         'ordered_attributes' => true,
         'ordered_class_elements' => true,
         'ordered_types' => true,
+        'no_unused_imports' => true,
+        'yoda_style' => true,
+        'elseif' => true,
         'header_comment' => ['header' => 'Copyright Humbly Arrogant Software Limited 2023-2025.
                 
 Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.'],
