3fcdd5b786953ec28f5b09b37fe6a774dac94157
Iain <iain@billabear.com>
2025-01-10 15:42:19 +0000

    Add payment stats repository skeleton
    Automatic commit - 36a74608a80bcf5137f111e8504fff692aaed672

---
+++ src/BillaBear/Repository/Stats/PaymentStatsRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright Humbly Arrogant Software Limited 2023-2025.
+ *
+ * Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ */
+
+namespace BillaBear\Repository\Stats;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class PaymentStatsRepository implements PaymentStatsRepositoryInterface
+{
+    public function __construct(
+        #[Autowire('@doctrine.dbal.default_connection')]
+        private Connection $connection,
+    ) {
+    }
+
+    public function getDailyPaymentStatesForAMonth(): array
+    {
+        // TODO: Implement getDailyPaymentStatesForAMonth() method.
+    }
+
+    public function getMonthlyPaymentStatsForAYear(): array
+    {
+        // TODO: Implement getMonthlyPaymentStatsForAYear() method.
+    }
+
+    public function getYearlyPaymentStats(): array
+    {
+        // TODO: Implement getYearlyPaymentStats() method.
+    }
+}
