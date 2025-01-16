c75b508f4d1cd989925a5c32e8b87c3bb6e33845
Iain <iain@humblyarrogant.io>
2025-01-16 10:48:20 +0100

    Update copyright header

--- src/BillaBear/Exception/Integrations/UnexpectedErrorException.php
+++ src/BillaBear/Exception/Integrations/UnexpectedErrorException.php
@@ -3,7 +3,7 @@
 /*
  * Copyright Humbly Arrogant Software Limited 2023-2025.
  *
- * Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ * Use of this software is governed by the Fair Core License, Version 1.0, ALv2 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  */
 
 namespace BillaBear\Exception\Integrations;
