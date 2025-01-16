7e54649af714f8a7f3f944d0257c25b20c7fe40d
Iain <iain@humblyarrogant.io>
2025-01-16 10:47:45 +0100

    Update license php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,7 @@
 /*
  * Copyright Humbly Arrogant Software Limited 2023-2025.
  *
- * Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
+ * Use of this software is governed by the Fair Core License, Version 1.0, ALv2 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.
  */
 
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
@@ -25,7 +25,7 @@ return (new PhpCsFixer\Config())
         'elseif' => true,
         'header_comment' => ['header' => 'Copyright Humbly Arrogant Software Limited 2023-2025.
                 
-Use of this software is governed by the Functional Source License, Version 1.1, Apache 2.0 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.'],
+Use of this software is governed by the Fair Core License, Version 1.0, ALv2 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.'],
     ])
     ->setFinder($finder)
     ->setParallelConfig(ParallelConfigFactory::detect())
