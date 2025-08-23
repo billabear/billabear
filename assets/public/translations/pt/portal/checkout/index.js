c1bfd3585b19f96089239874360c99def5861ce0
Marcus Vance <227830304+marcus-vance@users.noreply.github.com>
2025-08-23 13:34:16 +0000

    Merge c684875129c1bb2ecd4a0ba42359bc4567cbc927 into 3c56952181ccab65719e0252606193884043989b
    Automatic commit - 41d7f7706f55a8975959a7ba4ecef3eadf5a0b95

---
+++ assets/public/translations/pt/portal/checkout/index.js
@@ -0,0 +1,46 @@
+export const CHECKOUT_TRANSLATIONS = {
+    title: "Finalizar a compra",
+    total: "Total devido hoje: {amount} {currency}",
+    tax_total: "Imposto : {amount} {currency}",
+    items: {
+        title: "Discriminação de itens"
+    },
+    lines: {
+        description: "Descrição",
+        tax_total: "Imposto Total",
+        total: "Total",
+        tax_rate: "Taxa de imposto",
+    },
+    success: {
+        title: "Sucesso",
+        message: "Obrigado pelo vosso pagamento!"
+    },
+    customer: {
+        title: "Informação ao cliente",
+        fields: {
+            email: "Correio eletrónico",
+            street_line_one: "Linha de rua 1",
+            street_line_two: "Linha de rua 2",
+            city: "Cidade",
+            region: "Região",
+            country: "País",
+            post_code: "Código postal",
+        },
+        help_info: {
+            email: "O correio eletrónico a utilizar para a faturação"
+        },
+        submit: "Proceder ao pagamento"
+    },
+    payment: {
+        title: "Pagamento",
+        submit: "Pagar"
+    },
+    loading: "Carregar detalhes do checkout",
+    error: "Checkout indisponível",
+    schedule: {
+        week: "Semanal",
+        month: "Mensal",
+        year: "Anual",
+        "one-off": "Uma vez",
+    }
+}
