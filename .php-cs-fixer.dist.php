f35dbb8b102e0a3067bb4bc889bd49805ad23baf
Iain Cambridge <iain@iain.rocks>
2026-02-07 12:56:42 +0100

    Bump year

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -23,7 +23,7 @@ return (new PhpCsFixer\Config())
         'no_unused_imports' => true,
         'yoda_style' => true,
         'elseif' => true,
-        'header_comment' => ['header' => 'Copyright Iain Cambridge 2023-2025.
+        'header_comment' => ['header' => 'Copyright Iain Cambridge 2023-2026.
                 
 Use of this software is governed by the Fair Core License, Version 1.0, ALv2 Future License included in the LICENSE.md file and at https://github.com/BillaBear/billabear/blob/main/LICENSE.'],
     ])
